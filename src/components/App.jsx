e033c62abe910319555621f3f7da2a17e80de4bc
Sheila Stephen <sheila.je@gmail.com>
2019-03-01 17:18:12 -0800

    add route to teh error page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import InputForm from './InputForm';
+import Error404 from './Error404';
+import { Switch, Route } from 'react-router-dom';
 
 function App(){
   return(
     <div>
-      <InputForm/>
+      {/* <InputForm/> */}
+      <Switch>
+        <Route exact path='/' component={InputForm} />
+        <Route component={Error404} />
+      </Switch>
     </div>
   );
 }
