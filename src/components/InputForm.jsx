a858f7fac64a387bb5864a5c6c6ec21cda5a9e37
Sheila Stephen <sheila.je@gmail.com>
2019-03-01 17:16:06 -0800

    align the form

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
+import './assets/styles/styles.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 function InputForm(){
   return(
-    <div>
-      <form onSubmit={this.handleClick}>
-        <h3>Todays Weather</h3><br></br>
-        <div className="row">
-          <div className="col-md-4"><label>City</label></div>
-          <div className="col-md-8"><input type="text" name="city" className="form-control"/><br></br></div>
-          <div className="col-md-4"><label>Country</label></div>
-          <div className="col-md-8"><input type="text" name="country" className="form-control"/><br></br></div>
-          <div className="col-md-10"></div>
-          <div className="col-md-2"><button className="btn btn-info">Get Weather</button><br></br><br></br></div>
-        </div>
-      </form>
+    <div className='weatherContainer'>
+      <Form>
+        <Form.Group controlId="formBasicCity">
+          <Form.Label className="labelText">City</Form.Label>
+          <Form.Control type="text" placeholder="Enter city" className="city"/>
+          <Form.Text className="text-muted">
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicCountry">
+          <Form.Label className="labelText">Country</Form.Label>
+          <Form.Control type="text" placeholder="Enter country"  className="city"/>
+          <Form.Text className="text-muted">
+          </Form.Text>
+        </Form.Group>
+
+       
+        <Button className ="btn" variant="warning">Get the Weather</Button>
+       
+        
+      </Form>
     </div>
   );
 }
