bace6dc62eb17d7ab31b7141d9e71197c7b06ca4
Sheila Stephen <sheila.je@gmail.com>
2019-03-01 17:17:36 -0800

    add error component

---
+++ src/components/Error404.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+function Error404(props) {
+  return (
+    <div>
+      <p>The page {props.location.pathname} does not exist.</p>
+    </div>
+  );
+}
+
+Error404.propTypes = {
+  location: PropTypes.object
+};
+
+export default Error404;
