8a191d89fa5c0790669c00c02f8671fdd0831750
Sheila Stephen <sheila.je@gmail.com>
2019-03-01 17:15:20 -0800

    add package for styling and teh rules for styling

--- webpack.config.js
+++ webpack.config.js
@@ -1,84 +1,90 @@
-const webpack = require('webpack');
-const { resolve } = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const webpack = require('webpack')
+const {
+  resolve
+} =    require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
 
-    entry: [
-        'react-hot-loader/patch',
-        'webpack-dev-server/client?http://localhost:8080',
-        'webpack/hot/only-dev-server',
-        resolve(__dirname, 'src', 'index.jsx')
-    ],
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    resolve(__dirname, 'src', 'index.jsx')
+  ],
 
-    output: {
-        filename: 'app.bundle.js',
-        path: resolve(__dirname, 'build'),
-        publicPath: '/'
+  output: {
+    filename: 'app.bundle.js',
+    path: resolve(__dirname, 'build'),
+    publicPath: '/'
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+
+  devtool: '#source-map',
+
+  devServer: {
+    hot: true,
+    contentBase: resolve(__dirname, 'build'),
+    publicPath: '/'
+  },
+
+  module: {
+    rules: [{
+      test: /\.(png|gif|jp(e*)g|svg)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 8000,
+          name: 'images/[hash]-[name].[ext]'
+        }
+      }
     },
-
-    resolve: {
-        extensions: ['.js', '.jsx']
+    {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]
     },
-
-    devtool: '#source-map',
-
-    devServer: {
-        hot: true,
-        contentBase: resolve(__dirname, 'build'),
-        publicPath: '/'
+    {
+      test: /\.jsx?$/,
+      enforce: 'pre',
+      loader: 'eslint-loader',
+      exclude: /node_modules/,
+      options: {
+        emitWarning: true,
+        configFile: './.eslintrc.json'
+      }
     },
-
-    module: {
-        rules: [{
-                test: /\.(png|gif|jp(e*)g|svg)$/,
-                use: {
-                    loader: 'url-loader',
-                    options: {
-                        limit: 8000,
-                        name: 'images/[hash]-[name].[ext]'
-                    }
-                }
-            },
-            {
-                test: /\.jsx?$/,
-                enforce: 'pre',
-                loader: 'eslint-loader',
-                exclude: /node_modules/,
-                options: {
-                    emitWarning: true,
-                    configFile: './.eslintrc.json'
-                }
-            },
-            {
-                test: /\.jsx?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                options: {
-                    presets: [
-                        ['es2015', {
-                            'modules': false
-                        }],
-                        'react',
-                    ],
-                    plugins: [
-                        'react-hot-loader/babel',
-                        'styled-jsx/babel'
-                    ]
-                }
-            }
+    {
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      options: {
+        presets: [
+          ['es2015', { 'modules': false }],
+          'react',
+        ],
+        plugins: [
+          'react-hot-loader/babel',
+          'styled-jsx/babel'
         ]
-    },
+      }
+    }
+    ],
+  },
 
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NamedModulesPlugin(),
-        new HtmlWebpackPlugin({
-            template: 'template.ejs',
-            appMountId: 'react-app-root',
-            title: 'React Help Queue',
-            filename: resolve(__dirname, 'build', 'index.html'),
-        }),
-    ]
-};
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'template.ejs',
+      appMountId: 'react-app-root',
+      title: 'Portfolio',
+      filename: resolve(__dirname, 'build', 'index.html'),
+    }),
+  ]
+}
