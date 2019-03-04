528b4a860ebab8baab9a68fe977698a30ae910ec
Sheila Stephen <sheila.je@gmail.com>
2019-03-04 06:40:19 -0800

    add styles

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -5,18 +5,24 @@ import Button from 'react-bootstrap/Button';
 
 function InputForm(){
   return(
+     
     <div className='weatherContainer'>
-      <Form>
-        <Form.Group controlId="formBasicCity">
+
+      <p className="formHeading" for="">Find out the Weather !!</p> <br></br>
+      <Form className="gridStyles">
+
+        {/* <p className="formHeading" for="">Find out the Weather !!</p> <br></br> */}
+
+        <Form.Group controlId="formBasicCity" classname="cityTextStyle">        
           <Form.Label className="labelText">City</Form.Label>
           <Form.Control type="text" placeholder="Enter city" className="city"/>
           <Form.Text className="text-muted">
           </Form.Text>
         </Form.Group>
 
-        <Form.Group controlId="formBasicCountry">
+        <Form.Group controlId="formBasicCountry" classname="cityTextStyle">
           <Form.Label className="labelText">Country</Form.Label>
-          <Form.Control className="textBoxStyle" type="text" placeholder="Enter country"  className="city"/>
+          <Form.Control type="text" placeholder="Enter country"  className="city"/>
           <Form.Text className="text-muted">
           </Form.Text>
         </Form.Group>
