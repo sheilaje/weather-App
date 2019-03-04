555ab907a075b36fba1abd8d7cbe6afbda0de135
Sheila Stephen <sheila.je@gmail.com>
2019-03-04 15:56:11 -0800

    make API call from the App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,43 @@
 import React from 'react';
 import InputForm from './InputForm';
 import Error404 from './Error404';
+import appid from './../../.env';
 import { Switch, Route } from 'react-router-dom';
 
-function App(){
-  return(
-    <div>
-      {/* <InputForm/> */}
-      <Switch>
-        <Route exact path='/' component={InputForm} />
-        <Route component={Error404} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      masterList: []
+     };
+     this.handleNewFormSubmission = this.handleNewFormSubmission.bind(this);
+  }
+
+
+  handleNewFormSubmission(requestParams){
+    let url= "http://api.openweathermap.org/data/2.5/forecast?zip=" + requestParams.city + "&appid=" + process.env.REACT_APP_appid;
+    fetch(url)
+      .then(result => result.json())
+      .then(
+        (result) => {
+        console.log(result);
+        },
+        (error) => {
+          console.log(error)
+        }
+      )
+  }
+
+  render () {
+    return(
+      <div>
+        <Switch>
+          <Route exact path='/' render={()=><InputForm onNewFormSubmission={this.handleNewFormSubmission}/>} />
+          <Route component={Error404} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
-
