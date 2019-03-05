c6f06ea546ae72b83102d7093622404003ee16a7
Sheila Stephen <sheila.je@gmail.com>
2019-03-04 16:49:24 -0800

    add display component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import InputForm from './InputForm';
+import DisplayWeather from './DisplayWeather'
 import Error404 from './Error404';
 import appid from './../../.env';
 import { Switch, Route } from 'react-router-dom';
@@ -8,6 +9,7 @@ class App extends React.Component{
   constructor(props){
     super(props);
     this.state = {
+      apiResult: [],
       masterList: []
      };
      this.handleNewFormSubmission = this.handleNewFormSubmission.bind(this);
@@ -15,12 +17,13 @@ class App extends React.Component{
 
 
   handleNewFormSubmission(requestParams){
-    let url= "http://api.openweathermap.org/data/2.5/forecast?zip=" + requestParams.city + "&appid=" + process.env.REACT_APP_appid;
+    let url= "http://api.openweathermap.org/data/2.5/forecast?q=" + requestParams.city + "," + requestParams.country + "&appid=" + process.env.REACT_APP_appid;
     fetch(url)
       .then(result => result.json())
       .then(
         (result) => {
         console.log(result);
+        this.setState(apiResult: result);
         },
         (error) => {
           console.log(error)
@@ -35,9 +38,14 @@ class App extends React.Component{
           <Route exact path='/' render={()=><InputForm onNewFormSubmission={this.handleNewFormSubmission}/>} />
           <Route component={Error404} />
         </Switch>
+        <DisplayWeather/>
       </div>
     );
   }
+}
+
+App.propTypes={
+
 }
 
 export default App;
