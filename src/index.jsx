18a8c049476f951d949e06e5f460afac00164fb1
Sheila Stephen <sheila.je@gmail.com>
2019-03-12 09:46:34 -0700

    render <provider> , so that it makes the redux store available to any nested components that have been wrapped in the connect() function

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { createStore, applyMiddleware } from 'redux';
 import { HashRouter } from 'react-router-dom';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import middlewareLogger from './middleware/middleware-logger';
 import ForecastListReducer from './reducers/ForecastListReducer';
+import thunkMiddleware from 'redux-thunk';
 
-const store = createStore(ForecastListReducer);
+const store = createStore(ForecastListReducer, applyMiddleware(middlewareLogger,
+thunkMiddleware));
+
 
 const render = (Component) => {
-  ReactDOM.render(
-      <HashRouter>
-        <Provider store={store}>
-          <Component />
-        </Provider> 
-      </HashRouter>,
-    document.getElementById('react-app-root')
-  );
+ReactDOM.render(
+    <HashRouter>
+      <Provider store={store}>
+        <Component />
+      </Provider>
+    </HashRouter>,
+  document.getElementById('react-app-root')
+);
 };
 
 render(App);
+let unsubscribe = store.subscribe(()=>{
+  console.log('subscription', store.getState());
+})
