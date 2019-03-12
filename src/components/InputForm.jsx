f8024b5bc5e9c1b38b4f7eadb4116137afbce77b
Sheila Stephen <sheila.je@gmail.com>
2019-03-12 09:09:09 -0700

    update input form to pass the user typed city as a parameter to the action creators

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,39 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { createForecastAction } from '../actions';
 import './assets/styles/styles.css';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
 
-function InputForm(){
-  return(
-     
-    <div className='weatherContainer'>
+function InputForm({ dispatch }){
 
-      <p className="formHeading" for="">Find out the Weather !!</p> <br></br>
-      <Form className="gridStyles">
 
-        {/* <p className="formHeading" for="">Find out the Weather !!</p> <br></br> */}
-
-        <Form.Group controlId="formBasicCity" classname="cityTextStyle">        
-          <Form.Label className="labelText">City</Form.Label>
-          <Form.Control type="text" placeholder="Enter city" className="city"/>
-          <Form.Text className="text-muted">
-          </Form.Text>
-        </Form.Group>
-
-        <Form.Group controlId="formBasicCountry" classname="cityTextStyle">
-          <Form.Label className="labelText">Country</Form.Label>
-          <Form.Control type="text" placeholder="Enter country"  className="city"/>
-          <Form.Text className="text-muted">
-          </Form.Text>
-        </Form.Group>
-
-       
-        <Button className ="btn" variant="warning">Get the Weather</Button>
-       
-        
-      </Form>
-    </div>
-  );
+return (
+<div>
+<input type='text'
+       id='city'
+       ref={(input)=>{_city=input;}}
+       placeholder='city'/>
+<button type='button' onClick={getForecast} className='btn'> Get Forecast </button>
+</div>
+)
 }
 
-export default InputForm;
+InputForm.props={
+dispatch: PropTypes.func
+}
+
+export default connect()(InputForm);
