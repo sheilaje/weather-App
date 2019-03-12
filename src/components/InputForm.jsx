6fa73f8060ff20fe5ec6a53715d419366da51458
Sheila Stephen <sheila.je@gmail.com>
2019-03-12 09:15:22 -0700

    add method to handle button onClick event

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { createForecastAction } from '../actions';
+import { createForecastAction } from './actions';
 import './assets/styles/styles.css';
 
 function InputForm({ dispatch }){
 
+let _city=null;
+
+function getForecast(event){
+  event.preventDefault();
+  let city=_city.value;
+  dispatch(createForecastAction(city));
+}
 
 return (
 <div>
