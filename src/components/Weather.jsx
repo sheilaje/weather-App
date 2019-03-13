845df45cfa659922b8f2056b23cd39f6868262e2
Student <example@example.com>
2019-03-13 13:24:33 -0700

    add expansion panel from Material UI - for UI effects

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,84 +1,80 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Moment from 'moment';
 import ForecastDetail from './ForecastDetail';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-// import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import plus from './assets/images/plus-big-512.png';
+import { withStyles } from '@material-ui/core/styles';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-class Weather extends React.Component{
-  constructor(props){
+const styles = theme => ({
+  root: {
+    width: '100%',
+    margin: '10px',
+  },
+  heading: {
+    fontSize: '20px',
+    fontWeight: 'bold',
+    flexBasis: '23.33%',
+    flexShrink: 0,
+    padding: '15px 5px;',
+  },
+  imgHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: '13.33%',
+    color: theme.palette.text.secondary,
+  },
+  secondaryHeading: {
+    fontSize: '16px',
+    fontWeight: 'bold',
+    color: theme.palette.text.secondary,
+    padding: '15px 5px;',
+  },
+});
+
+class Weather extends React.Component {
+  constructor(props) {
     super(props);
-    this.state={open: false,
+    this.state = {
+      expanded: false,
     };
-    this.handleClickOpen=this.handleClickOpen.bind(this);
-    this.handleClose=this.handleClose.bind(this);
+    this.handleChange=this.handleChange.bind(this);
   }
 
-  handleClickOpen (){
-    this.setState({ open: true });
-  }
+  handleChange(){
+    this.setState({
+      expanded: this.state.expanded ? false : true
+    });
+  };
 
-  handleClose () {
-    this.setState({ open: false });
-  }
-
-
-  render(){//Button for handleOpen required to allow Button for handleClose to work
+  render() {
+    const { classes } = this.props;
+    const { expanded } = this.state;
     const today = this.props.forecast.forecastMoment.format('ddd');
     const { text, icon } = this.props.forecast.day.condition;
 
-    return (<div className="clearfix weatherRow">
-      <div className="weatherCol weatherDay">
-        <p className="roundedDay">{today}</p>
+    return (
+      <div className={classes.root}>
+        <ExpansionPanel expanded={this.state.expanded} onChange={this.handleChange}>
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography className={classes.heading}>{today}</Typography>
+            <Typography className={classes.imgHeading}><img src={icon} alt='weather icon'/></Typography>
+            <Typography className={classes.secondaryHeading}>{text}</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              <ForecastDetail forecast={this.props.forecast}/>
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
       </div>
-      <div className="weatherCol weatherTextSpan">{text} </div>
-      <div className="weatherCol weatherImg"><img src={icon} alt='weather icon'/></div>
-      <div className="weatherCol weatherPlus">
-        <Button variant="outlined" color="primary" onClick={this.handleClickOpen}>
-          <img src={plus} className="weatherPlusImg" alt='weather icon'/>
-        </Button>
-      </div>
-
-      <Dialog
-        open={this.state.open}
-        onClose={this.handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">{today}</DialogTitle>
-        <DialogContent>
-          <ForecastDetail forecast={this.props.forecast}/>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={this.handleClose} color="primary" autoFocus>
-            DONE
-          </Button>
-        </DialogActions>
-      </Dialog>
-
-    </div>);
+    );
   }
 }
 
 Weather.propTypes = {
-  forecast: PropTypes.shape({
-    forecastMoment: PropTypes.instanceOf(Moment).isRequired,
-    date: PropTypes.string,
-    date_epoch: PropTypes.number,
-    day: PropTypes.shape({
-      condition: PropTypes.shape({
-        text: PropTypes.string.isRequired,
-        icon: PropTypes.string,
-        code: PropTypes.number
-      }).isRequired
-    }),
-    astro: PropTypes.objectOf(PropTypes.string)
-  })
+  classes: PropTypes.object.isRequired,
 };
 
-export default Weather;
+export default withStyles(styles)(Weather);
